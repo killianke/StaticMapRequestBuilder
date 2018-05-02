14e85f7612e5bf34c8b03a90ef244e62b4b9a7f9
Killian Kenny <killiankenny21@gmail.com>
2018-05-03 00:38:36 +0100

    Creating request in sample controller and loading map image

--- Example/StaticMapRequestBuilder/ViewController.swift
+++ Example/StaticMapRequestBuilder/ViewController.swift
@@ -8,18 +8,27 @@
 
 import UIKit
 import Kingfisher
+import StaticMapRequestBuilder
+import MapKit
 
 class ViewController: UIViewController {
     
     let imageView = UIImageView()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view = imageView
         imageView.backgroundColor = .white
         imageView.contentMode = .scaleAspectFit
         
-        if let imageUrl = URL(string: "https://thumbs.dreamstime.com/b/tropical-fish-colourful-sixspine-butterfly-fish-7820672.jpg") {
+        let coordinate = CLLocationCoordinate2D(latitude: 52.980778, longitude: -6.046528)
+        let mapUrl = MapRequestBuilder(withSize: CGSize(width: 300, height: 200))
+            .addCenter(coordinate: coordinate)
+            .addZoom(20)
+            .build()
+        
+        print("Map URL: \(String(describing: mapUrl))")
+        if let imageUrl = mapUrl {
             imageView.kf.setImage(with: imageUrl)
         }
     }
