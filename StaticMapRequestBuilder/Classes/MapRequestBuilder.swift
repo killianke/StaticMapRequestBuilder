236c39d1a4e4e141eb2e149ce62690c341945620
Killian Kenny <killiankenny21@gmail.com>
2018-05-03 00:36:28 +0100

    Added MapRequestBuilder class

---
+++ StaticMapRequestBuilder/Classes/MapRequestBuilder.swift
@@ -0,0 +1,37 @@
+//
+//  StaticMapRequestBuilder.swift
+//  StaticMapRequestBuilder
+//
+//  Created by Killian-Kenny on 02/05/2018.
+//
+
+import Foundation
+import MapKit
+
+open class MapRequestBuilder {
+    
+    private var components = URLComponents(string: "https://maps.googleapis.com/maps/api/staticmap")
+    
+    public init(withSize size: CGSize) {
+        let valueString = "\(Int(size.width))x\(Int(size.height))"
+        let sizeQuery = URLQueryItem(name: "size", value: valueString)
+        components?.queryItems = [sizeQuery]
+
+    }
+    
+    open func addCenter(coordinate: CLLocationCoordinate2D) -> MapRequestBuilder {
+        let centerQuery = URLQueryItem(name: "center", value: "\(coordinate.latitude),\(coordinate.longitude)")
+        components?.queryItems?.append(centerQuery)
+        return self
+    }
+    
+    open func addZoom(_ level: Int) -> MapRequestBuilder {
+        let zoomQuery = URLQueryItem(name: "zoom", value: String(level))
+        components?.queryItems?.append(zoomQuery)
+        return self
+    }
+    
+    open func build() -> URL? {
+        return components?.url
+    }
+}
