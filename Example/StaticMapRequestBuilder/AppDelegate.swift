b3df4483be58efb9ed3ac38d127c36bb23987325
Killian Kenny <killiankenny21@gmail.com>
2018-05-02 23:38:37 +0100

    Set ViewController as root controller of window

--- Example/StaticMapRequestBuilder/AppDelegate.swift
+++ Example/StaticMapRequestBuilder/AppDelegate.swift
@@ -15,7 +15,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+
+        if let window = self.window {
+            window.makeKeyAndVisible()
+            window.rootViewController = ViewController()
+        }
+        
         return true
     }
 
