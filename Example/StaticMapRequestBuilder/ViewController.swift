23c6ac97144e38c30d86f7fa9e4ba983dab7cf6e
Killian Kenny <killiankenny21@gmail.com>
2018-05-02 23:40:05 +0100

    Setting image view as controller view, loading image using Kingfisher

--- Example/StaticMapRequestBuilder/ViewController.swift
+++ Example/StaticMapRequestBuilder/ViewController.swift
@@ -7,12 +7,21 @@
 //
 
 import UIKit
+import Kingfisher
 
 class ViewController: UIViewController {
+    
+    let imageView = UIImageView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = .purple
+        self.view = imageView
+        imageView.backgroundColor = .white
+        imageView.contentMode = .scaleAspectFit
+        
+        if let imageUrl = URL(string: "https://thumbs.dreamstime.com/b/tropical-fish-colourful-sixspine-butterfly-fish-7820672.jpg") {
+            imageView.kf.setImage(with: imageUrl)
+        }
     }
 
 }
